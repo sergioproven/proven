fe51d9f9586288658401005231154e70df13ee4e
Sergio gomez <sergio_7110@hotmail.com>
2014-10-10 08:48:18 +0200

    confirmacion

---
+++ ImageManipulation.java
@@ -0,0 +1,29 @@
+/* 
+ * To change this template, choose Tools | Templates 
+ * and open the template in the editor. 
+ */ 
+package com.daw.model; 
+
+import java.awt.Color; 
+import java.awt.Graphics2D; 
+import java.awt.Rectangle; 
+import java.awt.RenderingHints; 
+import java.awt.image.BufferedImage; 
+import java.awt.image.ColorModel; 
+import java.awt.image.WritableRaster; 
+import java.io.File; 
+import java.io.IOException; 
+import javax.imageio.ImageIO; 
+// import org.imgscalr.Scalr; 
+// import org.imgscalr.Scalr.Method; 
+
+/** 
+ * 
+ * @author alumne 
+ */ 
+public class ImageManipulation { 
+    
+    public ImageManipulation() { 
+         
+    } 
+}
